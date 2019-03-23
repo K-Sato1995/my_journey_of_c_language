7eeecfe14d4b039d49d3342bebc3d82b7356a055
Katsuki <Katsuki_Sato0130@icloud.com>
2019-03-23 11:39:44 +0900

    Add array

---
+++ Array/array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  // Defining an array
+  int numbers[5]; // type name_of_array[size]
+
+  // Assigning values
+  numbers[0] = 0;
+  numbers[1] = 1;
+  numbers[2] = 2;
+
+  // Using a value from an array
+  printf("num: %d\n", numbers[0]); //=> num: 0
+  return 1;
+}
