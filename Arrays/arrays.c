defbe1352a5c6947d6dacb93a4aa825803037acc
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-25 18:42:57 +0900

    Update arrays

--- Arrays/arrays.c
+++ Arrays/arrays.c
@@ -9,7 +9,11 @@ int main() {
   numbers[1] = 1;
   numbers[2] = 2;
 
+  // Define an array with the initial values.
+  int numbers2[2] = { 1, 2 };
+
   // Using a value from an array
   printf("num: %d\n", numbers[0]); //=> num: 0
+  printf("num: %d\n", numbers2[1]);
   return 1;
 }
