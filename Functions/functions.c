ed912966a2a395a469c14448d1008f6154dc9f1f
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-29 21:05:12 +0900

    Update Functions

--- Functions/functions.c
+++ Functions/functions.c
@@ -2,13 +2,20 @@
 
 // プロトタイプ宣言
 int dob(int number);
+char l(char n);
 
 int main() {
   printf("2の2倍は%d\n", dob(2));
+  printf("Print %c\n", l('a'));
   return 0;
 }
 
 // Define a function
 int dob(int number) {
   return number * 2;
+}
+
+//
+char l(char n) {
+  return n;
 }
