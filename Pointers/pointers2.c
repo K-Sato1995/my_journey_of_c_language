c5c87a3cfabca2842283fd4a324f3a557eb786a8
Katsuki <Katsuki_Sato0130@icloud.com>
2019-06-03 20:39:35 +0900

    Update Pointers

--- Pointers/pointers2.c
+++ Pointers/pointers2.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-  // ポインタ型の配列を宣言する。
-  int *arr[2];
+  int x;
+  int *px;
+  x = 10;
 
+  // アドレスを表示する。
+  px = &x;
+
+  printf("%p\nn", px);
+  //=> 0x7fff5a9b7758
+
+  // 配列のアドレスを表示する。
+  int arr[3];
+  int *p;
+
+  // 配列名そのものが先頭のアドレスを表すので、&が不要。
+  p = arr;
+
+  printf("%p\n", p);
+  //=> n0x7fff5a9b775c
 }
