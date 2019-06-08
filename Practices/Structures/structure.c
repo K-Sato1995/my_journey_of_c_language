2e7563878e50acc7e19aacef3194ab22ec754193
Katsuki <Katsuki_Sato0130@icloud.com>
2019-06-08 10:59:20 +0900

    Add more code

--- Practices/Structures/structure.c
+++ Practices/Structures/structure.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+// Declaring a struct
+typedef struct grade {
+  int number;
+  char *name;
+  double ave;
+  char grade;
+}GRADE;
+
 int main(void){
   struct person {
     char name[10];
     char sex;
     int age;
   };
-
   struct person ken = {"Ken", 'M', 10};
+  printf("My name is %s. I'm %d.\n", ken.name, ken.age); //=> My name is Ken. I'm 10.
 
-  printf("My name is %s. I'm %d.\n", ken.name, ken.age);
+
+  GRADE data1 = {1001, "James Lee", 77.3, 'B'};
+  printf("%d %s %f %c \n", data1.number, data1.name, data1.ave, data1.grade); //=> 1001 James Lee 77.300000 B
 }
