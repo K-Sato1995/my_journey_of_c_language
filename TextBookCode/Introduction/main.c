49ce1ebf18e66ff9f6da71f64c2673ffdfdeb67b
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-13 15:39:38 +0900

    Add more code

--- TextBookCode/Introduction/main.c
+++ TextBookCode/Introduction/main.c
@@ -10,4 +10,31 @@ int main(void) {
   int age = 24;
   char *name = "K-Sato";
   printf("name: %s, age: %d\n", name, age);
+
+  // 配列
+  int arr[3] = {1, 2, 3};
+  int i;
+  for(i =0; i < 3; i++) {
+    printf("%d", arr[i]);
+  }
+
+  // 2次元配列
+  int arr2[2][3] = {
+    { 1, 2, 3 },
+    { 4, 5, 6 }
+  };
+  for(i =0; i < 2; i++) {
+    printf("%d", arr2[i][i]); //=> 1 5
+  }
+  printf("\n");
+
+  // 文字列
+  char str[10] = "Hello";
+  printf("%s\n", str);
+
+  // 文字列の配列
+  char strArr[2][10] = { "Bye", "World" };
+  for(i = 0; i < 2; i++) {
+    printf("%s\n", strArr[i]);
+  }
 }
