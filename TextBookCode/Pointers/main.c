f1d0d1d3a1362e11145d06d4f6fab1486370bf31
Katsuki <Katsuki_Sato0130@icloud.com>
2019-08-12 09:46:48 +0900

    Add Pointers to Textbook code

---
+++ TextBookCode/Pointers/main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(void) {
+  int *p;
+  int num = 5;
+
+  p = &num;
+
+  printf("The memory address of num is %p\n", p); //=> The memory address of num is 0x7fff5db1a7c4
+  printf("The value of num is %d\n", *p); //=> The value of num is 5
+
+
+  int *pointer;
+  int arr[3] = {10, 20, 30};
+  int i;
+  pointer = arr;
+  printf("The size of int is %lu\n", sizeof(int)); //=> The bite of int is 4
+
+  for(i=0; i < 3; i++, pointer++) {
+    printf("*pointer = %d\n", *pointer);
+    printf("arr[%d] = %d\n", i, arr[i]);
+  }
+  /*
+  *pointer = 10
+  arr[0] = 10
+  *pointer = 20
+  arr[1] = 20
+  *pointer = 30
+  arr[2] = 30
+  */
+}
